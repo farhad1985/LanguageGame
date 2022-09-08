0f7ec36c276fdb382164c7411fc01868657be66d
Sherlock <farhad.public@gmail.com>
2022-09-08 18:24:25 +0200

    create project

---
+++ LanguageGame/LanguageGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LanguageGame
+//
+//  Created by Farhad on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
