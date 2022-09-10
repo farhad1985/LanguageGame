d95f126e117ed116befe8933c5dde4044f76b319
Sherlock <farhad.public@gmail.com>
2022-09-10 11:57:41 +0200

    implement timer

--- LanguageGame/LanguageGame/Application/Home/HomeViewModel.swift
+++ LanguageGame/LanguageGame/Application/Home/HomeViewModel.swift
@@ -14,29 +14,74 @@ class HomeViewModel {
     @Published var currentWord: RandomWord?
     @Published var correctAttempts: Int = 0
     @Published var wrongAttempts: Int = 0
+    @Published var isFinished: Bool = false
+    @Published var secondsRemaining = 0
+
+    var gameTime: Int { return 5 }
     
+    private var timer: Timer?
     private let totalQuestions = 15
     private let validWrongAttempts = 3
     
-
     init(randomWordGameLogic: RandomWordGameLogic) {
         self.randomWordGameLogic = randomWordGameLogic
     }
     
-    func answer(isCorrect: Bool) {
-        if (currentWord?.isCorrect ?? false) == isCorrect {
+    func answer(isCorrect: Bool, isTimeOut: Bool = false) {
+        
+        if isTimeOut {
+            wrongAttempts += 1
+            
+        } else if (currentWord?.isCorrect ?? false) == isCorrect {
             correctAttempts += 1
+            
         } else {
             wrongAttempts += 1
+            
         }
         
-        currentWord = randomWordGameLogic.fetchNewWord()
+        if wrongAttempts + correctAttempts == totalQuestions ||
+            wrongAttempts == validWrongAttempts {
+            
+            isFinished = true
+            
+        } else {
+            currentWord = randomWordGameLogic.fetchNewWord()
+        }
     }
 
     func resetGame() {
+        isFinished = false
         correctAttempts = 0
         wrongAttempts = 0
         randomWordGameLogic.resetWords()
         currentWord = randomWordGameLogic.fetchNewWord()
+        startTimer()
+    }
+    
+    func restTimer() {
+        secondsRemaining = gameTime
+    }
+    
+    private func startTimer() {
+        secondsRemaining = gameTime
+        
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(updateTimer),
+            userInfo: nil,
+            repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            secondsRemaining -= 1
+            
+        } else {
+            secondsRemaining = gameTime
+            answer(isCorrect: false, isTimeOut: true)
+        }
     }
 }
